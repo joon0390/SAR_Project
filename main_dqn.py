a94f3593dcd0841e6951a4d1ad5f49d3328dbf78
kwonheedam <164458040+kwonheedam@users.noreply.github.com>
2024-07-26 23:48:14 +0900

    Update main_dqn.py

--- main_dqn.py
+++ main_dqn.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 from geo_processing import GISProcessor, load_shapefiles
 from reward import RewardCalculator
-from method.Dqn import DQN, dqn_learning, simulate_path, load_model,Agent
+from Dqn import  dqn_learning, simulate_path, load_model,Agent,DQN
 from config import *
 from utils import show_path_with_arrows, get_random_point_within_polygon
 
@@ -26,25 +26,26 @@ if __name__ == "__main__":
     road_transformed = combined_array[:, :, 4]
     watershed_basins_transformed = combined_array[:, :, 5]
     channels_transformed = combined_array[:, :, 6]
+    forestroad_transformed = combined_array[:,:,7]
+    hiking_transformed = combined_array[:,:,8]
 
     # 보상 계산기 인스턴스 생성
-    reward_calculator = RewardCalculator(dem_array, rirsv_transformed, wkmstrm_transformed, climbpath_transformed, road_transformed, watershed_basins_transformed, channels_transformed)
+    reward_calculator = RewardCalculator(dem_array, rirsv_transformed, wkmstrm_transformed, climbpath_transformed, road_transformed, watershed_basins_transformed, channels_transformed,forestroad_transformed,hiking_transformed)
     
-    action_mode = '8_directions'  # or '8_directions'
+    action_mode = 'custom'  # or '8_directions'
 
     # Agent 인스턴스 생성∆
     agent = Agent(age_group='young', gender='male', health_status='good')
 
     # DQN 학습 수행
-    dqn_learning(dem_array, rirsv_transformed, wkmstrm_transformed, climbpath_transformed, road_transformed, watershed_basins_transformed, channels_transformed, reward_calculator, agent, action_mode=action_mode)
+    dqn_learning(dem_array, rirsv_transformed, wkmstrm_transformed, climbpath_transformed, road_transformed, watershed_basins_transformed, channels_transformed,forestroad_transformed, hiking_transformed,reward_calculator, agent, action_mode=action_mode)
 
     # 경로 시뮬레이션 예시
     #start_x, start_y = get_random_point_within_polygon(area_difference_file, dem_array.shape)
-    start_x, start_y = 2545,3154
-    model = load_model('dqn_model.pth', input_dim=10, output_dim=8 if action_mode == '8_directions' else 6)
-    path = simulate_path(start_x, start_y, model, dem_array, rirsv_transformed, wkmstrm_transformed, climbpath_transformed, road_transformed, watershed_basins_transformed, channels_transformed, agent, action_mode=action_mode)
+    start_x, start_y = 2546,3155
+    model = load_model('dqn_model.pth', input_dim=12, output_dim=8 if action_mode == '8_directions' else 6)
+    path = simulate_path(start_x, start_y, model, dem_array, rirsv_transformed, wkmstrm_transformed, climbpath_transformed, road_transformed, watershed_basins_transformed, channels_transformed, forestroad_transformed,hiking_transformed,agent, action_mode=action_mode)
     
     print("Simulated Path:")
     print(path)
-
     show_path_with_arrows(dem_array, path)
