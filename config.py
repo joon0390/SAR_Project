f672cca305f243dbcf6087891c022a039a5aa859
kwonheedam <164458040+kwonheedam@users.noreply.github.com>
2024-07-28 15:59:13 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,16 @@
-# 파일 경로 설정
-dem_file = '/Users/heekim/Desktop/heekimjun/WiSAR/data/GIS 데이터_20240704/dem/dem.tif'
-area_file = '/Users/heekim/Desktop/heekimjun/WiSAR/data/GIS 데이터_20240704/area/area.shp'
-rirsv_shp_file = '/Users/heekim/Desktop/heekimjun/WiSAR/data/GIS 데이터_20240704/river/it_c_rirsv.shp'
-wkmstrm_shp_file = '/Users/heekim/Desktop/heekimjun/WiSAR/data/GIS 데이터_20240704/river/lt_c_wkmstrm.shp'
-climbpath_shp_file = '/Users/heekim/Desktop/heekimjun/WiSAR/data/GIS 데이터_20240704/road/lt_l_frstclimb.shp'
-road_shp_file = '/Users/heekim/Desktop/heekimjun/WiSAR/data/GIS 데이터_20240704/road/road_polygon.shp'
-watershed_basins_shp_file = '/Users/heekim/Desktop/heekimjun/WiSAR/data/GIS 데이터_20240704/watershed/3/validBasins3.shp'
-channels_shp_file = '/Users/heekim/Desktop/heekimjun/WiSAR/data/GIS 데이터_20240704/watershed/3/channel3.shp'
+dem_file= '/Users/kwonheedam/Desktop/data/GIS_데이터_20240704/dem/dem.tif'
+area_file = '/Users/kwonheedam/Desktop/data/GIS_데이터_20240704/area/area.shp'
+rirsv_shp_file = '/Users/kwonheedam/Desktop/data/GIS_데이터_20240704/river/it_c_rirsv.shp'
+wkmstrm_shp_file = '/Users/kwonheedam/Desktop/data/GIS_데이터_20240704/river/lt_c_wkmstrm.shp'
+climbpath_shp_file = '/Users/kwonheedam/Desktop/data/GIS_데이터_20240704/road/lt_l_frstclimb.shp'
+road_shp_file = '/Users/kwonheedam/Desktop/data/GIS_데이터_20240704/road/road_polygon.shp'
+watershed_basins_shp_file = '/Users/kwonheedam/Desktop/data/GIS_데이터_20240704/watershed/basins_valid.shp'
+channels_shp_file = '/Users/kwonheedam/Desktop/data/GIS_데이터_20240704/watershed/channels.shp'
 q_table_file = '/Users/heekim/Documents/GitHub/SAR_Project/q_table.pkl'
-area_difference_file = "/Users/heekim/Desktop/heekimjun/WiSAR/data/GIS 데이터_20240704/area/area_difference.shp"
-
+forestroad_shp_file = '/Users/kwonheedam/Desktop/data/GIS_데이터_20240704/forestroad/clipped_31.shp' #추가
+hiking_shp_file = '/Users/kwonheedam/Desktop/data/GIS_데이터_20240704/climb/ulsan_climb_path.shp' #추가
+area_difference_file = "/Users/kwonheedam/Desktop/data/GIS_데이터_20240704/area/area_difference.shp"
+test_area_npy = '/Users/kwonheedam/Desktop/SAR_Project/test_area_result.npy'
 # 하이퍼 파라미터 설정
 alpha = 0.2  # 학습률
 gamma = 0.9  # 할인 인자
@@ -18,9 +19,9 @@ speed = 10  # 이동 속도
 
 beta = 0.1  # 불확실성에 대한 가중치
 
-max_steps = 1000  # 최대 스텝 수
-episodes = 100  # 에피소드 수
-simulation_max_steps = 2000 # 시뮬레이션 최대 스템
+max_steps = 10  # 최대 스텝 수
+episodes = 10  # 에피소드 수
+simulation_max_steps = 20 # 시뮬레이션 최대 스템
 
 target_update = 10
 batch_size = 16
